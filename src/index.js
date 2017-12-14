8c0b2f5d38b440b37f8fc520d1bdd69414917d45
Timothy Kim <tkim1202@gmail.com>
2017-12-14 18:11:43 +0900

    Added first react-router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Hello extends Component {
+  render() {
+    return <div>Hello!</div>
+  }
+}
+
+class Goodbye extends Component {
+  render() {
+    return <div>Goodbye!</div>
+  }
+}
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/hello" component={Hello}/>
+        <Route path="/goodbye" component={Goodbye}/>
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
